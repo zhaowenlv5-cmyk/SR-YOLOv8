0db1d9869597a993bb471b1966bc748ecbc7f534
zhaowenlv5-cmyk <zhaowenlv5@gmail.com>
2026-01-23 15:40:16 +0800

    Update test.py

--- test.py
+++ test.py
@@ -1,7 +1,8 @@
 from ultralytics.models import YOLO
 
 if __name__ == '__main__':
-    model = YOLO(r"D:/lzw/code/trained_model/CCTSDB2021/best.pt", task='detect')
-    model.val(data="CCTSDB2021vedai.yaml", workers=0, batch=1, name="val_CCTSDB2021", imgsz=512, device='0')  # train the model
+    model = YOLO(r"CCTSDB2021.pt", task='detect')
+    model.val(data="CCTSDB2021vedai.yaml", workers=0, batch=1, name="val", imgsz=512, device='0')  # train the model
 # 这里用了cosine learing rate，默认的是linear
+
 
